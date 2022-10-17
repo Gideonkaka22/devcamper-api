cec5300fb70023fa27b608d73160842093dacc78
Gideon Ugwumba <gideon.ugwumba@ccsnl.com>
2022-10-17 11:46:48 +0100

    Added readme and starter config

---
+++ routes/bootcamps.js
@@ -0,0 +1,38 @@
+const { Router } = require('express');
+const express = require('express');
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp,
+    bootcampPhotoUpload
+ } = require('../controllers/bootcamps');
+
+ // Include other resource routers
+ const courseRouter = require('./courses');
+ const reviewRouter = require('./reviews')
+
+
+const router = express.Router();
+
+const { protect, authorize } = require('../middleware/auth');
+
+//Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
+
+router.route('/:id/photo').put(protect, authorize('publisher', 'admin'),
+ bootcampPhotoUpload);
+
+router
+.route('/')
+.get(getBootcamps)
+.post(protect, authorize('publisher', 'admin'), createBootcamp);
+
+router.route('/:id')
+ .get(getBootcamp)
+ .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+ .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+
+module.exports = router;
